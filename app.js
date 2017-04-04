a2e0ec42a9214653e3c58efdb70815ca785f6d53
Iryna Maslova <maslovai@hotmail.com>
2017-04-03 21:12:51 -0700

    started on a contructor function

---
+++ app.js
@@ -0,0 +1,15 @@
+
+function Item(name){
+  this.href = name.href;
+  this.src  = name.src;
+  this.p  = name.paragraph;
+}
+
+
+
+
+
+// <a href="">
+//   <img id = "img-gallery" src="images/tulips.jpg" alt="">
+//   <p>Image caption</p>
+// </a>
