dab8031a94828b67d43b1db45111534e3faed7b4
Iryna Maslova <maslovai@hotmail.com>
2017-04-05 20:26:38 -0700

    added toggle between hamburger and nav links

--- app.js
+++ app.js
@@ -6,8 +6,18 @@ function Item(name){
 }
 
 
+   $("#hamburger").on("click", function(){
+     $(this).css("visibility", "hidden");
+     $("#nav").css("visibility", "visible");
+   })
 
-
+var handleMainNav = function(){
+  $('#nav-responsive').on('click', '.tab', function(e) {
+    e.preventDefault();
+    $('#wrapper').hide();
+    $('"#my-' + $(this).class + '"').fadeIn();
+  });
+}
 
 // <a href="">
 //   <img id = "img-gallery" src="images/tulips.jpg" alt="">
