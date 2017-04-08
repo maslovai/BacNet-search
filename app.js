275e4a7b18d7befb7332311d4ff3ffe87332503a
Iryna Maslova <maslovai@hotmail.com>
2017-04-08 12:06:55 -0700

    fadeIn works!!!!!

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 $(function(){
 
-function Item(name){
-  this.a = name.a;
-  this.src  = name.src;
-  this.p  = name.paragraph;
-}
+
+var theTemplateScript = $("#gallery-item-template").html();
+
+  // Compile the template
+  var theTemplate = Handlebars.compile(theTemplateScript);
+
+  // Define our data object
+  var context={
+    "href": "www.github.com/maslovai/portfolio",
+    "src": "images/tulips.jpg",
+    "p": "This is my portfolio on github"
+  };
+
+  // Pass our data to the template
+  var theCompiledHtml = '<li>'+theTemplate(context)+'</li>';
+
+  // Add the compiled html to the page
+  $('#gallery').append(theCompiledHtml);
+
+
+
+
+  // <a href = "{{}}">
+  //   <img id="img-gallery" src = "{{}}">
+  //   <p> {{}}</p>
+
+
+
+
 
  var pageView = {};
 
@@ -17,11 +41,11 @@ pageView.handleMainNav = function(){
   $('#nav-responsive').on("click", "a", function(e) {
     e.preventDefault();
     //console.log(e.target);
-    $('#wrapper').hide();
+    $('#wrapper').find('div').hide();
     //$('#wrapper').fadeIn();
     var toID = '#'+ $(this).attr("class");
     console.log(toID);
-    $('toID').fadeIn();
+    $(toID).fadeIn();
     //$('#wrapper').fadeIn();
     //$(this).parent().next().fadeIn(200).children(toID);
 
