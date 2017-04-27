891a5df246b14cc633e506dc4698eb1b0c0d6520
Iryna Maslova <maslovai@hotmail.com>
2017-04-26 19:53:32 -0700

    initiated npm

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// TODO: Initialize your project using NPM to create and populate a package.json file
+// Done -Iryna
+// TODO: Require the Express package that you installed via NPM, and instantiate the app
+// Done
+// const express = require('express');
+// const app = express();
+
+const PORT = process.env.PORT || 3000;
