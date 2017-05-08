741ba20e7bd755c104ab6f76e1a5f53586f45041
Iryna Maslova <maslovai@hotmail.com>
2017-05-08 14:19:15 -0700

    added filter()

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 $(function(){
 
-// using Handlebars to create gallery items
-
-var theItem = Handlebars.compile($("#gallery-item-template").html());
-  // console.log(itemContent);
-  // console.log (theItem(itemContent));
-  // for (var i=0; i<itemContent.length; i++){
-  //   $('#gallery').append(theItem(itemContent[i]));
-  // }
-  itemContent.map(ele =>{
-    $('#gallery').append(theItem(ele))
-  });
-   $("#hamburger").on("click", function(){
+$("#hamburger").on("click", function(){
      $(this).toggle();
      $("#nav-responsive").css("visibility","visible");
    })
+
+var theItem = Handlebars.compile($("#gallery-item-template").html());
+itemContent.filter(ele => {
+  if (ele.number<=2) return ele;
+}).map(ele =>{
+  $('#gallery').append(theItem(ele))
+});
 pageView.handleMainNav();
+
+
 });
--- projects_data.js
+++ projects_data.js
@@ -1,15 +1,18 @@
 var itemContent=[{
   url: "https://github.com/maslovai/portfolio",
   link: "images/tulips.jpg",
-  paragraph:"This is my portfolio on github"
+  paragraph:"This is my portfolio on github",
+  number:1
 },
 {
   url: "https://maslovai.github.io/bus-mall/",
   link: "images/Screen Shot 2017-04-08 at 2.53.23 PM.png",
-  paragraph: "This is a 201 project on github"
+  paragraph: "This is a 201 project on github",
+  number:2
 },
 {
   url: "https://mattreyes7.github.io/201-final-project/",
   link: "images/Screen Shot 2017-04-12 at 3.36.56 PM.png",
-  paragraph: "This is a 201 final group project"
+  paragraph: "This is a 201 final group project",
+  number:3
 }];
