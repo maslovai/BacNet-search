865604db18a91e462b8c69a798ef63cc2c9597ab
Iryna Maslova <maslovai@hotmail.com>
2017-05-17 21:34:50 -0700

    request working!

--- scripts/queryView.js
+++ scripts/queryView.js
@@ -12,30 +12,36 @@
     $('#nav-links #query-request-tab').hide().siblings().show();
   }
   query.getString = function(hospital, barcode, callback){
-    $.ajax({
-      url: `/entries`,
-      method: 'GET',
-      data: {
-        site:hospital,
-        barcode:barcode
-      }
-    }).then(callback)
+     console.log(hospital, barcode);
+    $.get('/entries/'+hospital+'/'+barcode)
+    // + '&barcode='+barcode)
+    .then(callback);
+    });
   }
-  let outputList = [];
-  $('#submit').on('click', function(){
+  // handling submit:
+  var outputList = [];
+  let $hospital;
+  let $barcode;
+   query.submitRequest = function() {
+    $('#bac-form').on('click', '#submit', function(e) {
+    e.preventDefault();
     $('#result-ul').empty();
-    let $hospital = $("#hospital-filter").val();
-    let $barcode = $("#bacCode").val();
+    $hospital = $("#hospital-filter").val();
+    $barcode = $("#bacCode").val();
+    //console.log($hospital, $barcode);
     //creating an array of list items after returnig a string form the DB
-    let outputList = getString($hospital, $barcode, callback).split('\n');
+    //don't know how to pass hospital and barcode into the get request
+    outpuList = query.getString($hospital, $barcode, callback);
+    outputList.split('\n');
     outputList.map(ele =>
     $('#result-ul').append(`<li>`+ ele + `</li>`));
-});
+})
+}
 
  $('#reset').on('click', function(){
   $('#hospital-filter').val("All").attr("selected","true");
   $('#sequence').val('').attr("placeholder","barcode");
  });
-
+query.submitRequest();
 module.query = query;
 })(window);
--- server.js
+++ server.js
@@ -23,12 +23,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-app.get('/entries', (request, response) => {
+app.get('/entries/:site/:barcode', (request, response) => {
+   //console.log(request.params);
   client.query(`SELECT * FROM entries
-      WHERE site=$1 and barcode=$2;
-    `,[request.site,request.barcode]
+      WHERE site=$1 AND barcode=$2;`,
+    [request.params.site,request.params.barcode]
   ).then(function(result) {
-    response.send(dataString);
+    console.log(result);
+    response.send(result.rows)
   })
   .catch(function(err) {
     console.error(err)
@@ -39,44 +41,45 @@ app.get('/entries', (request, response) => {
 
 
 
+// This was a test table.  We can delete it later.
+
+// function loadData() {
+//   client.query('SELECT COUNT(*) FROM all_data')
+//   .then(function(result) {
+//     if(!parseInt(result.rows[0].count)) {
+//       fs.readFile('data-test/data.json', function(err, fd) {
+//         console.log(err);
+//         JSON.parse(fd.toString()).forEach(function(ele) {
+//           client.query(`
+//             INSERT INTO
+//             all_data(hospital, bacteria, antibiotic, recommended, resistance)
+//             VALUES ($1, $2, $3, $4, $5);`,
+//             [ele.hospital, ele.bacteria, ele.antibiotic, ele.recommended, ele.resistance]
+//           )
+//         })
+//       })
+//     }
+//   })
+// };
 
 
-function loadData() {
-  client.query('SELECT COUNT(*) FROM all_data')
-  .then(function(result) {
-    if(!parseInt(result.rows[0].count)) {
-      fs.readFile('data-test/data.json', function(err, fd) {
-        console.log(err);
-        JSON.parse(fd.toString()).forEach(function(ele) {
-          client.query(`
-            INSERT INTO
-            all_data(hospital, bacteria, antibiotic, recommended, resistance)
-            VALUES ($1, $2, $3, $4, $5);`,
-            [ele.hospital, ele.bacteria, ele.antibiotic, ele.recommended, ele.resistance]
-          )
-        })
-      })
-    }
-  })
-};
-
-function loadDB(){
-  console.log('creating table');
-  client.query(`
-    CREATE TABLE IF NOT EXISTS
-    all_data(
-      item_id SERIAL PRIMARY KEY,
-      hospital VARCHAR(255) NOT NULL,
-      bacteria VARCHAR(5) NOT NULL,
-      antibiotic VARCHAR(20) NOT NULL,
-      recommended VARCHAR(4) NOT NULL,
-      resistance VARCHAR(6) NOT NULL);`
-  )
-  .then(function(data) {
-     loadData(data);
-  })
-  .catch(function(err) {
-    console.error(err)
-  });
-}
-loadDB();
+//function loadDB(){
+//   console.log('creating table');
+//   client.query(`
+//     CREATE TABLE IF NOT EXISTS
+//     all_data(
+//       item_id SERIAL PRIMARY KEY,
+//       hospital VARCHAR(255) NOT NULL,
+//       bacteria VARCHAR(5) NOT NULL,
+//       antibiotic VARCHAR(20) NOT NULL,
+//       recommended VARCHAR(4) NOT NULL,
+//       resistance VARCHAR(6) NOT NULL);`
+//   )
+//   .then(function(data) {
+//      loadData(data);
+//   })
+//   .catch(function(err) {
+//     console.error(err)
+//   });
+// }
+// loadDB();
