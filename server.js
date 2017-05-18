931a8d32e27dfea24a2869064b5c73510acee6e5
Maks <pvl1720@gmail.com>
2017-05-17 21:35:38 -0700

    fixed pages links with Ira

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const pg = require('pg');
 const fs = require('fs');
-const conString = 'postgres://irynamaslova@localhost:5432/antibiotics';
+const conString = 'postgres://maks@localhost:5432/antibiotics';
+/lab08
 
 const client = new pg.Client(conString);
 client.connect();
