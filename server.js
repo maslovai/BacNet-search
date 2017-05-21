ccb142a0f5e23269440d0e7968602b4f840a7d85
Iryna Maslova <maslovai@hotmail.com>
2017-05-20 22:19:59 -0700

    added postgres

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const fs = require('fs');
 
 //const conString = 'postgres://kev:32167@localhost:5432/antibiotics';
 // const conString = 'postgres://maks@localhost:5432/antibiotics';
-const conString = 'postgres://irynamaslova@localhost:5432/antibiotics';
+const conString = server.env.DATABASE_URL||'postgres://irynamaslova@localhost:5432/antibiotics';
 
 
 const client = new pg.Client(conString);
