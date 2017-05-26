668d28af5d129fac71d494515943b9f50938973a
Iryna Maslova <maslovai@hotmail.com>
2017-05-25 20:21:36 -0700

    checking if git works

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ const page = require('page');
 const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 app.use(express.static(__dirname + '/'));
-// const conString = process.env.DATABASE_URL || 'postgres://irynamaslova@localhost:5432/postgres';
-// const client = new pg.Client(conString);
-// client.connect();
-// client.on('error', err => console.error(err));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -26,5 +22,13 @@ function proxyGitHub(request, response) {
 app.get('*', (req, res) => {
   res.sendFile('index.html', { root: '.' })
 });
+
 app.get('/github/*', proxyGitHub);
 app.listen(PORT);
+
+
+
+// const conString = process.env.DATABASE_URL || 'postgres://irynamaslova@localhost:5432/postgres';
+// const client = new pg.Client(conString);
+// client.connect();
+// client.on('error', err => console.error(err));
