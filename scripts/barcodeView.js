2644dc97b021b7fd3951f819328ded044c279070
Iryna Maslova <maslovai@hotmail.com>
2017-06-12 18:22:34 -0700

    trying to pull form master

--- scripts/barcodeView.js
+++ scripts/barcodeView.js
@@ -56,6 +56,7 @@ const barcode = {};
 
    barcode.reset = function() {
      $('#barcodeRecord').val('');
+     $('#barcodeEntry').val('').attr('placeholder','1 2 3 4 5 6 7 8');
    }
   module.barcode = barcode;
 })(window);
